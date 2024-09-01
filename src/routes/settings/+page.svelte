e72c032c5030534e6fd59c5de4530a6317e89d97
Giacomo Nanni <gn.nanni@gmail.com>
2024-09-01 12:00:09 +0200

    settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -9,32 +9,31 @@
     <a href="/history">History</a>
 </nav>
 <nav>
-    <a href="/question">Write a new question</a> -
-    <a href="/ask">Select a question</a> -
+    <a href="/question">Write a new question</a> —
+    <a href="/ask">Select a question</a> —
 </nav>
 
 <slot />
 
 <style>
     nav {
+        font-family:
+            Arial Narrow,
+            Helvetica,
+            sans-serif;
         display: flex;
         gap: 10px;
         background-color: rgb(254, 255, 231);
         padding: 10px;
         margin: 10px;
         border-radius: 3px;
-        /* text-transform: uppercase; */
-        /* position: sticky;
-        top: 10px; */
-        /* z-index: 10; */
+        font-size: 3vw;
+        line-height: 1.2em;
+        color: black;
     }
 
     a {
         color: black;
         text-decoration: none;
-    }
-    a:hover {
-        text-decoration: underline dotted;
-        text-underline-offset: 4px;
     }
 </style>
